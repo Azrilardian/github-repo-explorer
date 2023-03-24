49a7343d01e5146248543dce2bb87ba98f41ccf2
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2023-03-24 17:33:26 +0800

    feat(global): add more dependencies and test adding more dependencies like tailwind, redux, and mui. Adding jest testing to

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Home from "@/pages/index";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    expect(screen.getByTestId("heading")).toBeInTheDocument();
+  });
+});
