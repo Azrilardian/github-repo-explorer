bb9bc27700df6928b0aa70eb52a63a7c45cd22bd
Azril Ardian Ifansyah <irfan05ariell@gmail.com>
2023-03-28 12:55:02 +0800

    feat(app-info): adding app info component adding add info component with using mui typography component

---
+++ src/components/app-info/app-info.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+const AppInfo = () => (
+  <>
+    <Typography variant="h1" gutterBottom fontSize={30} fontWeight={500}>
+      GITHUB REPO EXPLORER
+    </Typography>
+    <Typography variant="body1" gutterBottom my={2}>
+      Search your favorite github repo
+    </Typography>
+  </>
+)
+
+export default AppInfo
--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Input from '@/components/input/Input'
+import AppInfo from '@/components/app-info/app-info'
 import { AppContainer } from './app.elements'
 
 const App = () => (
   <AppContainer>
+    <AppInfo></AppInfo>
     <Input input="must-be-input" />
   </AppContainer>
 )
